8d7d50a8b3b1ff8b5657f2981ba4f7e68af17dfa
princeoo635 <princeoo635@gmail.com>
2025-02-16 10:46:41 +0530

    logout route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express";
-import {registerUser,loginUser} from "../controllers/user.controller.js";
+import {registerUser,loginUser,logoutUser} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router=Router();
 router.route("/register").post(
@@ -17,4 +18,7 @@ router.route("/register").post(
     registerUser);
 
 router.route("/login").post(loginUser)
+router.route("/logout").post(verifyJWT,logoutUser)
+
+
 export default router;
