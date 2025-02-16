5ff7aca029b33d7da692fb899908d2f7900e384d
princeoo635 <princeoo635@gmail.com>
2025-02-16 10:45:55 +0530

    bug fixed

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { User } from "../model/User.model.js";
+import { User } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
 
