25ab7c72cb26bb0c03e4ad767865f40e08c33f0a
princeoo635 <princeoo635@gmail.com>
2025-02-17 09:08:14 +0530

    token generation bug fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,10 +7,10 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 const generateAccessAndRefreshToken=async (userId)=>{
     try {
         const user=await User.findById(userId)
-        const accessToken=user.generateAccessToken()
-        const refreshToken=user.generateRefreshToken()
+        const accessToken=await user.generateAccessToken()
+        const refreshToken=await user.generateRefreshToken()
 
-        await User.save({validateBeforeSave: false})
+        await user.save({validateBeforeSave: false})
         return {accessToken,refreshToken}
     } catch (error) {
        throw new ApiError(500,"Something went wrong while generating refresh and access token") 
@@ -76,7 +76,7 @@ const registerUser=asyncHandler(
 
 const loginUser=asyncHandler(async (req,res,next)=>{
     const {username,email,password}=req.body;
-    if(!username || !email){
+    if(!username && !email){
         throw new ApiError(400,"username or email is required!")
     }
 
@@ -88,13 +88,13 @@ const loginUser=asyncHandler(async (req,res,next)=>{
         throw new ApiError(404,"User does not exists.")
     }
 
-    const isPasswordValid=await User.ispasswordCorrect(password)
+    const isPasswordValid=await user.isPasswordCorrect(password)
     if(!isPasswordValid){
         throw new ApiError(401,"Invalid user credentials")
     }
 
     const {accessToken,refreshToken}=await generateAccessAndRefreshToken(user._id)
-
+    
     const loggedInUser=await User.findById(user._id)
     .select("-password -refreshToken")
 
@@ -110,7 +110,7 @@ const loginUser=asyncHandler(async (req,res,next)=>{
         new ApiResponse(
             200,
             {
-                user,loggedInUser,accessToken,refreshToken
+                loggedInUser,accessToken,refreshToken
             },
             "User Successfully logged in!"
         )
@@ -140,5 +140,5 @@ const logoutUser=asyncHandler(
 
     }
 )
-
+ 
 export {registerUser,loginUser ,logoutUser}
