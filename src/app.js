a2782c49e8b8c60fe72f77efeb7cf78fd9869b0a
princeoo635 <princeoo635@gmail.com>
2025-04-10 11:32:33 +0530

    added api router for playlist

--- src/app.js
+++ src/app.js
@@ -16,8 +16,10 @@ app.use(cookieParser())
 // import routes
 import userRouter from "./routes/user.routes.js"
 import videoRouter from "./routes/video.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
 app.use("/api/v1/users",userRouter)
 app.use("/api/v1/videos",videoRouter)
+app.use("/api/v1/playlists",playlistRouter)
 
 
 export {app};
